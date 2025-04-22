66e037204ea09b10297837c45b2efd0823de434c
MMastroliindo <153086592+MMastroliindo@users.noreply.github.com>
2025-04-22 20:14:17 +0200

    Installed Django Rest Framework

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backendtriptales.apps.BackendtriptalesConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
