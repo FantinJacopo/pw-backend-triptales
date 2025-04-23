b4eb5762c0f8698f8c1c29ebdf7206ebd1a6edb8
MMastroliindo <153086592+MMastroliindo@users.noreply.github.com>
2025-04-23 17:35:00 +0200

    Modified urls.py

--- backendtriptales/urls.py
+++ backendtriptales/urls.py
@@ -1,12 +1,26 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from .views import (
+    CommentViewSet,
+    BadgeViewSet,
+    UserBadgeViewSet,
+    PostLikeViewSet,
+)
 from .views import UserRegistrationView
+router = DefaultRouter()
+router.register(r'comments', CommentViewSet)
+router.register(r'badges', BadgeViewSet)
+router.register(r'user-badges', UserBadgeViewSet)
+router.register(r'post-likes', PostLikeViewSet)
+
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
 ]
--- backendtriptales/views.py
+++ backendtriptales/views.py
@@ -1,7 +1,27 @@
-from rest_framework import generics
+from rest_framework import viewsets, generics
 from rest_framework.permissions import AllowAny
-from .models import User
-from .serializers import UserRegistrationSerializer
+
+from .models import Comment, Badge, UserBadge, PostLike, User
+from .serializers import CommentSerializer, BadgeSerializer, UserBadgeSerializer, PostLikeSerializer, \
+    UserRegistrationSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+class BadgeViewSet(viewsets.ModelViewSet):
+    queryset = Badge.objects.all()
+    serializer_class = BadgeSerializer
+
+class UserBadgeViewSet(viewsets.ModelViewSet):
+    queryset = UserBadge.objects.all()
+    serializer_class = UserBadgeSerializer
+
+class PostLikeViewSet(viewsets.ModelViewSet):
+    queryset = PostLike.objects.all()
+    serializer_class = PostLikeSerializer
+
 
 class UserRegistrationView(generics.CreateAPIView):
     queryset = User.objects.all()
